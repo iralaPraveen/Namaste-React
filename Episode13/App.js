31b5ae8dba78a81f21a83a8b0eb6a08fef5a47d8
irala praveen <iralapraveenofficial@gmail.com>
2025-07-13 11:19:59 +0530

    Testing using jest

---
+++ Episode13/App.js
@@ -0,0 +1,3 @@
+alert("hi we are connected");
+
+confirm("hi we are conforming the details");
---
+++ Episode13/__tests__/sum.test.js
@@ -0,0 +1,7 @@
+import { sum } from "../sum";
+
+test("These function is to calculate sum", () => {
+    const result = sum(1, 2,)
+    
+    expect(result).toBe(3)
+})
